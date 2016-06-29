b72d492eaddcb61ba8617c04583c5920b732a873
robinborst95 <robinborst95@gmail.com>
2016-06-29 13:25:13 +0200

    Added functionality for sliding through multiple pointclouds.

--- Potree_result/examples/deltagoot.js
+++ Potree_result/examples/deltagoot.js
@@ -1,5 +1,12 @@
 var sceneProperties = {
-	path: "../resources/pointclouds/deltagoot_0/cloud.js",
+	paths: [
+		"../resources/pointclouds/deltagoot_0/cloud.js",
+		"../resources/pointclouds/deltagoot_2/cloud.js",
+		"../resources/pointclouds/deltagoot_3/cloud.js",
+		"../resources/pointclouds/deltagoot_4/cloud.js",
+		"../resources/pointclouds/deltagoot_5/cloud.js",
+		"../resources/pointclouds/deltagoot_6/cloud.js"
+	],
 	cameraPosition: null, 		// other options: cameraPosition: [10,10,10],
 	cameraTarget: null, 		// other options: cameraTarget: [0,0,0],
 	fov: 60, 					// field of view in degrees,
